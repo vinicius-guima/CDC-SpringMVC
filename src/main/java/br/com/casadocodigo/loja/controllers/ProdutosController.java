2214b9d96d3fae6c110254a9d8e5faaa50ff519a
vinicius-guima <vinicius_guima.social@hotmail.com>
2020-05-24 18:31:31 -0300

    Cadastro produtos e seus preços

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -2,11 +2,12 @@ package br.com.casadocodigo.loja.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.casadocodigo.loja.daos.ProdutoDAO;
 import br.com.casadocodigo.loja.models.Produto;
+import br.com.casadocodigo.loja.models.TipoPreco;
 
 @Controller
 public class ProdutosController {
@@ -15,9 +16,10 @@ public class ProdutosController {
 	private ProdutoDAO produtoDao;
 
 	  @RequestMapping("/produtos/form")
-	    public String form(){
-		  System.out.println("form controller");
-	        return "produtos/form";
+	    public ModelAndView form(){
+		 ModelAndView modelAndview = new ModelAndView("/produtos/form");
+		 modelAndview.addObject( "tipos",TipoPreco.values());
+	        return modelAndview;
 	    }
 
 
--- src/main/java/br/com/casadocodigo/loja/models/Produto.java
+++ src/main/java/br/com/casadocodigo/loja/models/Produto.java
@@ -1,9 +1,13 @@
 package br.com.casadocodigo.loja.models;
 
+import java.util.List;
+
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
 
 @Entity
 public class Produto {
@@ -15,7 +19,17 @@ public class Produto {
 	private String titulo;
 	private String descricao;
 	private int paginas;
+	@ElementCollection
+	private List<Preco> precos;
 
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 
 	public String getTitulo() {
 		return titulo;
@@ -39,6 +53,14 @@ public class Produto {
 
 	public void setPaginas(int paginas) {
 		this.paginas = paginas;
+	}
+	
+	public List<Preco> getPrecos() {
+		return precos;
+	}
+
+	public void setPrecos(List<Preco> precos) {
+		this.precos = precos;
 	}
 
 	@Override
